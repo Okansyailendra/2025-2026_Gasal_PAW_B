f6cf8980115e13689cf75a5484131f2c29b87342
Okansyailendra <otansyailendra123@gmail.com>
2025-11-11 15:57:13 +0700

    add koneksi.php

---
+++ Modul 6/24-179_okan syailendra wahyudi/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+// koneksi.php
+$DB_HOST = '127.0.0.1';
+$DB_NAME = 'db_nota';
+$DB_USER = 'root';
+$DB_PASS = ''; // ubah jika pakai password
+
+try {
+    $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4", $DB_USER, $DB_PASS, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
